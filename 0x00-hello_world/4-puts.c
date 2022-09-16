730861ddb4aaa5ba5bf6b2ccb9f677429bd183bd
onyekachi <obijuruonyekachiesther@gmail.com>
2022-09-16 15:16:52 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stidio.h>
+
+/**
+ * *main - print
+ * *
+ * *Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
