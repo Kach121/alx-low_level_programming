2222c712c01612c64f3137647df8d865ad61690b
onyekachi <obijuruonyekachiesther@gmail.com>
2022-09-22 22:14:50 -0700

    task 12

--- 0x06-pointers_arrays_strings/print_buffer.c
+++ 0x06-pointers_arrays_strings/print_buffer.c
@@ -4,7 +4,7 @@
 /**
  * print_line -prints a s bytes of a buffer
  * @c: buffer to print
- * @s:bytes of buffer to print
+ * @s: bytes of buffer to print
  * @l: line of buffer to print
  *
  * Return: void
@@ -28,7 +28,7 @@ void print_line(char *c, int s, int l)
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-			putchsr('.');
+			putchar('.');
 	}
 }
 
