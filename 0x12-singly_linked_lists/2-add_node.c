0d6875a669bb330d424a8a0e33daae72ede59d98
onyekachi <obijuruonyekachiesther@gmail.com>
2022-10-21 08:52:10 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,8 +22,8 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = _strlen(str);
 	new->next = *head;
-	*head = new:
-		return (new);
+	*head = new;
+	return (new);
 }
 
 /**
